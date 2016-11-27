dd4eb88e5eace775a3e1dea0c73883acffca63f8
Zetra <zetra@mail.ugm.ac.id>
2016-11-27 23:55:16 +0700

    Show addres book frame with dummy data

--- src/com/zelory/kace/adressbook/Main.java
+++ src/com/zelory/kace/adressbook/Main.java
@@ -1,9 +1,24 @@
 package com.zelory.kace.adressbook;
 
-import com.zelory.kace.adressbook.view.AddressBookFrame;
+import com.zelory.kace.adressbook.data.model.AddressBook;
+import com.zelory.kace.adressbook.data.model.Person;
+import com.zelory.kace.adressbook.ui.AddressBookFrame;
+
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        new AddressBookFrame().setVisible(true);
+        AddressBook addressBook = new AddressBook();
+        for (int i = 0; i < 300; i++) {
+            Person person = new Person();
+            person.setFirstName("Orang");
+            person.setLastName("ke " + (i + 1));
+            person.setAddress("Suatu tempat");
+            person.setCity("Suatu kota");
+            person.setState("Suatu negara");
+            person.setZip(new Random().nextInt(9999) + "");
+            addressBook.getPersons().add(person);
+        }
+        new AddressBookFrame(addressBook).setVisible(true);
     }
 }
