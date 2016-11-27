dcc9fcb504cdd3c00469e95b8be2c046ffac2ab1
Zetra <zetra@mail.ugm.ac.id>
2016-11-28 01:13:28 +0700

    Remove dummy data

--- src/com/zelory/kace/adressbook/Main.java
+++ src/com/zelory/kace/adressbook/Main.java
@@ -1,23 +1,9 @@
 package com.zelory.kace.adressbook;
 
-import com.zelory.kace.adressbook.data.model.AddressBook;
-import com.zelory.kace.adressbook.data.model.Person;
 import com.zelory.kace.adressbook.ui.AddressBookFrame;
-import com.zelory.kace.adressbook.util.RandomUtil;
 
 public class Main {
     public static void main(String[] args) {
-        AddressBook addressBook = new AddressBook();
-        for (int i = 0; i < 300; i++) {
-            Person person = new Person();
-            person.setFirstName("Orang");
-            person.setLastName("ke " + (i + 1));
-            person.setAddress("Suatu tempat");
-            person.setCity("Suatu kota");
-            person.setState("Suatu negara");
-            person.setZip(RandomUtil.getInstance().randomInt(1000, 9999) + "");
-            addressBook.getPersons().add(person);
-        }
-        new AddressBookFrame(addressBook).setVisible(true);
+        new AddressBookFrame().setVisible(true);
     }
 }
